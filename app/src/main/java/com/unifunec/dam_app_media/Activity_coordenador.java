ee4976e25059a135a65cf7e2f64f03764b00cc9f
EstevanVenancio <estevanvenancio12@gmail.com>
2023-03-28 18:12:29 -0300

    Tela_Coordenador

---
+++ app/src/main/java/com/unifunec/dam_app_media/Activity_coordenador.java
@@ -0,0 +1,14 @@
+package com.unifunec.dam_app_media;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_coordenador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordenador2);
+    }
+}
