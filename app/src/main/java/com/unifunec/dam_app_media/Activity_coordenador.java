1acfc7a8761b2c4289faa62746b55855aeecf7f5
EstevanVenancio <estevanvenancio12@gmail.com>
2023-03-28 21:50:01 -0300

    Correção Scroll Rolagem TextView9

--- app/src/main/java/com/unifunec/dam_app_media/Activity_coordenador.java
+++ app/src/main/java/com/unifunec/dam_app_media/Activity_coordenador.java
@@ -3,6 +3,9 @@ package com.unifunec.dam_app_media;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.Scroller;
+import android.widget.TextView;
 
 public class Activity_coordenador extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class Activity_coordenador extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordenador);
+
+        TextView t9 = findViewById(R.id.textView9);
+
+        t9.setMovementMethod(new ScrollingMovementMethod());
+
     }
 }
