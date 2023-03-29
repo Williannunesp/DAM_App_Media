e9d309f6b293870467e57f91983173c395d0c0d5
EstevanVenancio <estevanvenancio12@gmail.com>
2023-03-29 15:48:06 -0300

    Criação Tela Cad Aluno

---
+++ app/src/main/java/com/unifunec/dam_app_media/Activity_cad_aluno.java
@@ -0,0 +1,14 @@
+package com.unifunec.dam_app_media;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_cad_aluno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_aluno);
+    }
+}
