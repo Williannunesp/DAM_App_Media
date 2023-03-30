ebdc11903314927835edaba5472d1efc1c115c42
EstevanVenancio <estevanvenancio12@gmail.com>
2023-03-30 00:32:33 -0300

    Tela Cadastro de Aluno
    Atualização dos campos nome, cpf e curso.

--- app/src/main/java/com/unifunec/dam_app_media/Activity_cad_aluno.java
+++ app/src/main/java/com/unifunec/dam_app_media/Activity_cad_aluno.java
@@ -1,6 +1,7 @@
 package com.unifunec.dam_app_media;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.viewmodel.CreationExtras;
 
 import android.os.Bundle;
 import android.view.View;
@@ -17,10 +18,26 @@ public class Activity_cad_aluno extends AppCompatActivity {
 
         Button salvar = findViewById(R.id.buttonSalvar);
 
+        EditText nome = findViewById(R.id.editTextCampoNome);
+        EditText cpf = findViewById(R.id.editTextCampoCpf);
+        EditText curso = findViewById(R.id.editTextCampoCurso);
+
         salvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(Activity_cad_aluno.this, "Cadastro realizado com sucesso!", Toast.LENGTH_LONG).show();
+
+                if(nome.getText().length()== 0){
+                    Toast.makeText(Activity_cad_aluno.this, "Preencha o campo nome!", Toast.LENGTH_LONG).show();
+                } else if (cpf.getText().length()== 0) {
+                    Toast.makeText(Activity_cad_aluno.this, "Preencha o campo CPF!", Toast.LENGTH_LONG).show();
+                } else if (curso.getText().length()== 0) {
+                    Toast.makeText(Activity_cad_aluno.this, "Preencha o campo curso!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Activity_cad_aluno.this, "Cadastro realizado com sucesso!", Toast.LENGTH_LONG).show();
+                    nome.setText("");
+                    cpf.setText("");
+                    curso.setText("");
+            }
            }
         });
     }
