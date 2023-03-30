a6c33bccefbd250a32e52b3c9b8939f050b836fd
WILLIAN PAES NUNES <107520609+Williannunesp@users.noreply.github.com>
2023-03-30 00:52:55 -0300

    Validação campos calculo_media

--- app/src/main/java/com/unifunec/dam_app_media/Activity_calculo_media.java
+++ app/src/main/java/com/unifunec/dam_app_media/Activity_calculo_media.java
@@ -1,12 +1,11 @@
 package com.unifunec.dam_app_media;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.viewmodel.CreationExtras;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,30 +30,38 @@ public class Activity_calculo_media extends AppCompatActivity {
         Button calcular = findViewById(R.id.btn_Calcular);
         Button limpar = findViewById(R.id.btn_Limpar);
 
-        nota1.setFocusable(true);
+
         calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-                if(imm.isActive())
-                    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
 
-                calcular.setEnabled(false);
-                limpar.setEnabled(true);
+                if (nota1.getText().toString().isEmpty() == true) {
+                    Toast.makeText(Activity_calculo_media.this, "Insira a Primeira Nota", Toast.LENGTH_LONG).show();
+                } else if (nota2.getText().toString().isEmpty() == true) {
+                    Toast.makeText(Activity_calculo_media.this, "Insira a Segunda Nota", Toast.LENGTH_LONG).show();
+                } else {
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    if (imm.isActive())
+                        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
 
-                double n1 = Double.parseDouble(nota1.getText().toString());
-                double n2 = Double.parseDouble(nota2.getText().toString());
+                    calcular.setEnabled(false);
+                    limpar.setEnabled(true);
 
-                double op = (n1+n2)/2;
-                String res = String.format("%.2f",op);
+                    double n1 = Double.parseDouble(nota1.getText().toString());
+                    double n2 = Double.parseDouble(nota2.getText().toString());
 
-                if(op>=7){
-                    ap.setText("APROVADO!");
-                    me.setText(res);
-                }else{
-                    rep.setText("REPROVADO!");
-                    me.setText(res);
+                    double op = (n1 + n2) / 2;
+                    String res = String.format("%.2f", op);
+
+
+                    if (op >= 7) {
+                        ap.setText("APROVADO!");
+                        me.setText(res);
+                    } else {
+                        rep.setText("REPROVADO!");
+                        me.setText(res);
+                    }
                 }
             }
         });
